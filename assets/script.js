a47c0c4056f0dcad09e1f42bfb0f6973d05ac851
danieldmota <danieldourado2102@gmail.com>
2026-01-07 13:31:19 -0400

    feat: add hamburguer button for navbar menu

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,28 @@
-  const buttons = document.querySelectorAll('.read-button');
+const buttons = document.querySelectorAll(".read-button");
 
-  buttons.forEach(button => {
-    button.addEventListener('click', () => {
-      const card = button.closest('.card');
-      card.classList.toggle('active');
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const card = button.closest(".card");
+    card.classList.toggle("active");
 
-      button.textContent = card.classList.contains('active') ? 'Ler menos' : 'Ler mais';
-    });
+    button.textContent = card.classList.contains("active")
+      ? "Ler menos"
+      : "Ler mais";
   });
+});
+
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("nav-bar");
+const icon = hamburger.querySelector("i");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("active");
+
+  if (menu.classList.contains("active")) {
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-xmark");
+  } else {
+    icon.classList.remove("fa-xmark");
+    icon.classList.add("fa-bars");
+  }
+});
